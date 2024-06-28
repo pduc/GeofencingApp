cbe9ba57370e951060c158581fb86d2bef88168d
Duc Pham <DucP@joblogic.com>
2024-06-28 21:07:04 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeofencingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
